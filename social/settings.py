edea37da10f95d2caf6ea26d78c9a8d9d7955d72
Tomiwa <calebomoniyitomiwa@gmail.com>
2023-02-28 16:57:30 +0100

    added  .keep file

--- social/settings.py
+++ social/settings.py
@@ -127,7 +127,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-# DISABLE_COLLECTSTATIC=1
+DISABLE_COLLECTSTATIC=1
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
