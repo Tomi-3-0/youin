b1ecfcd52976488957e74d77054747e15f7f03c2
Tomiwa <calebomoniyitomiwa@gmail.com>
2023-02-28 18:00:22 +0100

    ..

--- social/settings.py
+++ social/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://chattobi.herokuapp.com/', '127.0.0.1']
+ALLOWED_HOSTS = ['chattobi.herokuapp.com/', '127.0.0.1']
+# ALLOWED_HOSTS = ['*']
 
 
 # Application definition
